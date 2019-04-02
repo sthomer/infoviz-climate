54f79710ee5035425345a7a9607ffebb66c061f5
steven.t.homer@gmail.com <steven.t.homer@gmail.com>
2019-04-02 15:11:05 +0200

    info page

--- app/src/info.jsx
+++ app/src/info.jsx
@@ -5,10 +5,136 @@ export default class Info extends React.Component {
   render() {
     return (
       <>
-        <Title>Datasets</Title>
         <Title>Questions</Title>
+        Track 1: Effects of Humanity on Climate Change
+        <ul>
+          <li>
+            Which human activities have the most impact on climate change?
+          </li>
+          <li>
+            What countries/regions contribute most to these activities?
+          </li>
+          <li>
+            How are those places trying to mitigate their impact?
+          </li>
+          <li>
+            Are we screwed?
+          </li>
+        </ul>
+        Track 2: Effects of Climate Change on Humanity
+        <ul>
+          <li>
+            What consequences of climate change have the most impact on humanity?
+          </li>
+          <li>
+            What regions/habitats are most affected?
+          </li>
+          <li>
+            Are weather anomalies more frequent? Where? What are their effects?
+          </li>
+          <li>
+            Are we screwed?
+          </li>
+        </ul>
         <Title>Propositions</Title>
+        Track 1: Effects of Humanity on Climate Change
+        <ul>
+          <li>
+            CO2/other particulates effect on atmosphere
+          </li>
+          <li>
+            Carbon/Other Emissions according to specific industries e.g. oil, deforestation, farming
+          </li>
+          <li>
+            Countries/regions where these practices are most prevalent/egregious
+          </li>
+          <li>
+            Mitigation steps by governments/organizations
+          </li>
+          <li>
+            Climate projections according to different mitigation scenarios
+          </li>
+        </ul>
+        Track 2: Effects of Climate Change on Humanity
+        <ul>
+          <li>
+            Sea level rise, droughts, flooding, storm frequency
+          </li>
+          <li>
+            Countries/regions most affected by climate change effects/anomalies
+          </li>
+          <li>
+            Coral degradation, desertification, habitat loss
+          </li>
+          <li>
+            Hurricane/typhoon frequency/severity, flooding/drought severity
+          </li>
+          <li>
+            Climate projections and their consequencies on regions/habitats
+          </li>
+        </ul>
+        <Title>Datasets</Title>
+        Current:
+        <ul>
+          <li>
+            <a href="https://github.com/awesomedata/awesome-public-datasets#climate-weather">
+              AwesomeData: Climate + Weather
+            </a>: Primarily temperature and precipitation; also has projections
+          </li>
+          <li>
+            <a href="http://geodata.grid.unep.ch/">
+              United Nations Environmental Data
+            </a>: Habitat and disasters, others
+          </li>
+          <li>
+            <a href="https://www.gapminder.org/data/">
+              Gapminder
+            </a>: CO2 Data over time
+          </li>
+        </ul>
+        Searching:
+        <ul>
+          <li>
+            Government mitigation programs with projections
+          </li>
+        </ul>
+        <Title>Visualizations</Title>
+        Multi-modal Map
+        <ul>
+          <li>
+            World/Regional map that will display certain data depending on subject
+          </li>
+          <li>
+            e.g. display temperatures, anomalies, emissions, etc.
+          </li>
+        </ul>
+        Time Machine
+        <ul>
+          <li>
+            Given a certain data feature, view that over time (100 years?)
+          </li>
+          <li>
+            e.g. display plots and specific maps
+          </li>
+        </ul>
         <Title>Inspirations</Title>
+        <ul>
+          <li>
+            <a href="http://explorador.cr2.cl/">
+              Explorador Climatico
+            </a>: Multi-modal map
+          </li>
+          <li>
+            <a href="https://earthobservatory.nasa.gov/blogs/earthmatters/wp-content/uploads/sites/5/2016/09/tempanoms_gis_august2016.gif">
+              Earth Observatory
+            </a>: Time-varying plot
+          </li>
+          <li>
+            <a href="https://ourworldindata.org/natural-disasters">
+              Natural Disasters
+            </a>: Many different plots of natural disasters
+          </li>
+        </ul>
       </>
     );
   }
