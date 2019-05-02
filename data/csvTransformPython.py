9601c9157a608a36b1874b6169051b694d2fd048
Myxfall <maximilienromain96@hotmail.com>
2019-05-02 17:43:14 +0200

    [fix] cleaning dataset script

--- data/csvTransformPython.py
+++ data/csvTransformPython.py
@@ -2,7 +2,8 @@ import csv
 
 globalData = {}
 
-with open("GlobalTemp_TEST.csv") as csv_file:
+#with open("GlobalTemp_TEST.csv") as csv_file:
+with open("GlobalLandTemperaturesByCountry.csv") as csv_file:
 	reader = csv.reader(csv_file, delimiter=',')
 	line_count = 0
 
@@ -10,21 +11,24 @@ with open("GlobalTemp_TEST.csv") as csv_file:
 		if line_count == 0:
 			#globalData["colNames"] = row
 			globalData["dates"] = []
+			line_count += 1
+
 		else:
-			countryName = row[3]
-			if countryName in globalData:
-				globalData[countryName] += [row[1]]
-			else:
-				globalData[countryName] = [row[1]]
-			globalData["dates"] += [row[0]]
-		line_count += 1
+			# Sorting with minimal date
+			year = row[0].split("-")[0]
+			if year >= "1800":
+				countryName = row[3]
+				if countryName in globalData:
+					globalData[countryName] += [row[1]]
+				else:
+					globalData[countryName] = [row[1]]
+				if row[0] not in globalData["dates"]:
+					globalData["dates"] += [row[0]]
+
+
 csv_file.close()
 
-for elem in globalData:
-	print(elem,globalData[elem])
-
-
-with open('testCSV.csv', mode='w') as csv_file:
+with open('GlobalTempByCountryFrom1800.csv', mode='w') as csv_file:
 	writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	writer.writerow(['country'] + globalData["dates"])
 	del globalData["dates"]
