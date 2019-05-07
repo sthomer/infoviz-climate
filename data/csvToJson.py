1d217895570967f9b04298f1ea8023369194af36
Myxfall <maximilienromain96@hotmail.com>
2019-05-07 16:30:12 +0200

    [fix] better args management in py script

--- data/csvToJson.py
+++ data/csvToJson.py
@@ -1,28 +1,42 @@
 import csv
 import json
+import sys
 
-globalData = {}
+def main():
+	globalData = {}
+	
+	if (len(sys.argv) < 3):
+		print("Error: please enter input file and output file as argument")
+	else:
+		inputFile = sys.argv[1]
+		outputFile = sys.argv[2]
 
-#with open("GlobalTemp_TEST.csv") as csv_file:
-with open("GlobalTempByCountryFrom1800.csv") as csv_file:
-	reader = csv.reader(csv_file, delimiter=',')
-	line_count = 0
+		print("input file : ", inputFile)
+		print("output file : ", outputFile)
 
-	for row in reader:
-		if line_count == 0:
-			#globalData["colNames"] = row
-			globalData["dates"] = row
-			line_count += 1
+		#with open("GlobalTemp_TEST.csv") as csv_file:
+		with open(inputFile) as csv_file:
+			reader = csv.reader(csv_file, delimiter=',')
+			line_count = 0
 
-		else:
-			name = row[0]
-			dataTemp = row[1:]
+			for row in reader:
+				if line_count == 0:
+					#globalData["colNames"] = row
+					globalData["dates"] = row
+					line_count += 1
+
+				else:
+					name = row[0]
+					dataTemp = row[1:]
 
 
-			globalData[name] = dataTemp
+					globalData[name] = dataTemp
 
-csv_file.close()
+		csv_file.close()
 
-with open("GlobalTempByCountryFrom1800JSON.txt", "w") as jsonFile:
-	json.dump(globalData, jsonFile)	
+		with open(outputFile, "w") as jsonFile:
+			json.dump(globalData, jsonFile)	
+
+if __name__ == '__main__':
+  main()
 
