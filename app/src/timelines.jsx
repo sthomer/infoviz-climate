bbc17be7b475140a66e4593e983e5ea73dbab498
Fabian Perez <fabian.ramiro.perez@vub.be>
2019-05-07 16:11:47 +0200

    Adding time line graph.

--- app/src/timelines.jsx
+++ app/src/timelines.jsx
@@ -2,12 +2,60 @@ import React from 'react';
 import * as d3 from 'd3';
 import styled from 'styled-components';
 import { Title } from './styles';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
 
 export default class Timelines extends React.Component {
 
+  componentDidMount() {
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    chart.paddingRight = 20;
+
+    let data = [];
+    let visits = 10;
+    for (let j = 1; j < 119; j++) {
+      var year = 1900 + j
+      var date = new Date(year, 0, 0)
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+      data.push({ date: date, name: date, value: visits });
+  }
+
+    chart.data = data;
+
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    let series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+    series.tooltipText = "{valueY.value}";
+    chart.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    chart.scrollbarX = scrollbarX;
+
+    this.chart = chart;
+  }
+
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
   render() {
-    return (<>
-      <Title>Timelines</Title>
-    </>)
+    return (
+      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+    );
   }
 }
