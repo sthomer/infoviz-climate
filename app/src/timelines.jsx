efe08bd5991d70f37ecca4ad0fb314b1336f70c2
Fabian Perez <fabian.ramiro.perez@vub.be>
2019-05-09 02:42:18 +0200

    Adding selection functionality.

--- app/src/timelines.jsx
+++ app/src/timelines.jsx
@@ -9,12 +9,32 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 am4core.useTheme(am4themes_animated);
 
 export default class Timelines extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStartDate: undefined,
+      selectedendDate: undefined
+    };
+    var selectedComponent = undefined;
+  }
 
-  componentDidMount() {
-    let chart = am4core.create("chartdiv", am4charts.XYChart);
+  updateDate(startDate, endDate){
+    console.log(startDate + " " + endDate)
+    // this.props.selectedDate = startDate;
+    // this.props.selectedDate = endDate;
+  }
 
-    chart.paddingRight = 20;
+  updateDateString(str){
+    const array = str.split(" ");
+    let startDate = array[1]
+    let endDate = array[3]
+    console.log(startDate + " " + endDate)
+    // this.props.selectedDate = startDate;
+    // this.props.selectedDate = endDate;
 
+  }
+
+  loadData() {
     let data = [];
     let visits = 10;
     for (let j = 1; j < 119; j++) {
@@ -22,13 +42,24 @@ export default class Timelines extends React.Component {
       var date = new Date(year, 0, 0)
       visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
       data.push({ date: date, name: date, value: visits });
+    }
+    return data;
   }
 
-    chart.data = data;
+  componentDidMount() {
+    const updateDateBounds = this.updateDate
+    const updateDateBoundsString = this.updateDateString
+    var selectedComponent = undefined
+    var selectedDate = undefined
+
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    chart.paddingRight = 20;
+
+    chart.data = this.loadData();
 
     let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
     dateAxis.renderer.grid.template.location = 0;
-
 
     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
     valueAxis.tooltip.disabled = true;
@@ -40,11 +71,84 @@ export default class Timelines extends React.Component {
     series.tooltipText = "{valueY.value}";
     chart.cursor = new am4charts.XYCursor();
 
+    dateAxis.renderer.labels.template.cursorOverStyle = am4core.MouseCursorStyle.pointer;
+
     let scrollbarX = new am4charts.XYChartScrollbar();
     scrollbarX.series.push(series);
     chart.scrollbarX = scrollbarX;
 
+    scrollbarX.thumb.background.states.removeKey("hover");
+    scrollbarX.thumb.background.states.removeKey("down");
+    scrollbarX.thumb.background.fill = am4core.color("#07BEB8");
+    scrollbarX.thumb.background.fillOpacity = 0.4;
+    dateAxis.renderer.labels.template.events.on("hit", function(ev) {
+      if(selectedComponent != undefined){
+        selectedComponent.background.fill = am4core.color("white");
+        selectedComponent.fill = am4core.color("black");
+      }
+      scrollbarX.thumb.background.fill = am4core.color("#ACACAC");
+      scrollbarX.thumb.background.fillOpacity = 0.4;
+
+      ev.target.background.fill = am4core.color("#07BEB8");
+      ev.target.fill = am4core.color("white");
+      updateDateBounds(ev.target.text, ev.target.text)
+      selectedComponent = ev.target;
+      selectedDate = ev.target.text;
+    });
+
+    scrollbarX.events.on("rangechanged", function(ev) {
+      scrollbarX.thumb.background.fill = am4core.color("#07BEB8");
+      scrollbarX.thumb.background.fillOpacity = 0.4;
+
+      if(selectedComponent != undefined){
+        selectedComponent.background.fill = am4core.color("white");
+        selectedComponent.fill = am4core.color("black");
+        selectedComponent = undefined;
+      }
+      updateDateBoundsString(scrollbarX.thumb.titleElement.textContent);
+    });
+
+    scrollbarX.thumb.events.on("up", function(ev) {
+      scrollbarX.thumb.background.fill = am4core.color("#07BEB8");
+      scrollbarX.thumb.background.fillOpacity = 0.4;
+
+      if(selectedComponent != undefined){
+        selectedComponent.background.fill = am4core.color("white");
+        selectedComponent.fill = am4core.color("black");
+        selectedComponent = undefined;
+      }
+      updateDateBoundsString(scrollbarX.thumb.titleElement.textContent);
+    });
+
+
+    scrollbarX.scrollbarChart.events.on("up", function(ev) {
+      scrollbarX.thumb.background.fill = am4core.color("#07BEB8");
+      scrollbarX.thumb.group.node.children[0].fill = am4core.color("#07BEB8");
+      scrollbarX.thumb.background.fillOpacity = 0.3;
+      if(selectedComponent != undefined){
+        selectedComponent.background.fill = am4core.color("white");
+        selectedComponent.fill = am4core.color("black");
+      }
+      updateDateBoundsString(scrollbarX.thumb.titleElement.textContent);
+    });
+
+    chart.plotContainer.events.on("up", function(ev) {
+      scrollbarX.thumb.background.fill = am4core.color("#07BEB8");
+      scrollbarX.thumb.group.node.children[0].fill = am4core.color("#07BEB8");
+      scrollbarX.thumb.background.fillOpacity = 0.3;
+      if(selectedComponent != undefined){
+        selectedComponent.background.fill = am4core.color("white");
+        selectedComponent.fill = am4core.color("black");
+      }
+      updateDateBoundsString(scrollbarX.thumb.titleElement.textContent);
+    });
+
+
+
+    chart.chartCursor = false
     this.chart = chart;
+
+
   }
 
   componentWillUnmount() {
