d1f948bf0be1b3e14d6d7df5b215f9eeab7223ba
Myxfall <maximilienromain96@hotmail.com>
2019-05-14 16:06:58 +0200

    [fix] mean in dataset

--- data/csvToMeanCountriesPerYear.py
+++ data/csvToMeanCountriesPerYear.py
@@ -46,6 +46,34 @@ def main():
 		csv_file.close()
 		
 
+		"""# Global Temp transformation
+		for elem in globalData:
+			elemSize = len(globalData[elem])
+			if elemSize < sizeVector:
+				globalData[elem] = (["0"] * (sizeVector - elemSize - 1)) + globalData[elem]
+		
+		dates = globalData["dates"]
+		tmpDates = []
+		for i in range(0, len(dates), 12):
+			tmpDates += [dates[i][:4]]
+		globalData["dates"] = tmpDates
+
+		for elem in globalData:
+			if elem != "dates":
+				temps = globalData[elem]
+				meanTemp = []
+				for i in range(0, len(temps), 12):
+					tmp = []
+					for j in range(12):
+						if i+j < len(temps):
+							if temps[i+j] == "":
+								tmp += [0]
+							else:
+								tmp += [float(temps[i+j])]
+					mean = sum(tmp) / 12
+					meanTemp += [mean]
+				globalData[elem] = meanTemp"""
+
 		datesArray = globalData["dates"] 
 		del globalData["dates"]
 
@@ -54,15 +82,22 @@ def main():
 		newData["globalMean"] = []
 		sizeData = len(globalData)
 		for year in range(len(datesArray)):
+			sizeData = len(globalData)
 			tmpMean = 0.0
 			for elem in globalData:
 				dataArray = globalData[elem]
-				if dataArray[year] != "":
+				if dataArray[year] == 0:
+					sizeData -= 1
+				elif dataArray[year] == "":
+					sizeData -= 1
+				elif dataArray[year] != "":
 					tmpMean += float(dataArray[year])
+				
 			tmpMean /= sizeData	
+			print(tmpMean, " : ", sizeData)
 			newData["globalMean"] += [tmpMean]
 
-		print(newData)
+		#print(newData)
 
 
 		outputFile = inputFile[:len(inputFile)-4] + "GLOBALMEAN.json"
